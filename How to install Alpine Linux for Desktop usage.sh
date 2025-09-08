5c118b3ac3deeded8c5c11fd171fa9551efd2776
Vladimir Software Creator <info@srbobran48.rs>
2025-09-08 10:46:50 +0200

    Update How to install Alpine Linux for Desktop usage.sh

--- How to install Alpine Linux for Desktop usage.sh
+++ How to install Alpine Linux for Desktop usage.sh
@@ -3,45 +3,45 @@
 # Some parts you can execute using terminal!
 
 
-The Alpine Linux installation ISO uses root as the default user and an empty password. In order to login, just enter the username root and press return.
+# The Alpine Linux installation ISO uses root as the default user and an empty password. In order to login, just enter the username root and press return.
 
 
-[Initial password for root not exists, just press enter. There is no root password for the first time in Alpine Linux.]
+# [Initial password for root not exists, just press enter. There is no root password for the first time in Alpine Linux.]
 
-[You can setup root and user passwords later during the setup process.]
+# [You can setup root and user passwords later during the setup process.]
 
-[type = root]
-[login as root for the first time] 
+# [type = root]
+# [login as root for the first time] 
 
-[You can use su command to gain root access]
+# [You can use su command to gain root access]
 
 su
 setup-alpine
 
 
-us
-us
-localhost
-eth0
-UTC
-none
-none
-1
-none
-busybox
-sda
-sys
-y
+# us
+# us
+# localhost
+# eth0
+# UTC
+# none
+# none
+# 1
+# none
+# busybox
+# sda
+# sys
+# y
 
 
 
 reboot
 
-[remove installation DVD or Flash device]
+# [remove installation DVD or Flash device]
 
-[type = root]
+# [type = root]
 
-[login as root for the first time]
+# [login as root for the first time]
 
 
 
@@ -52,7 +52,7 @@ apk add nano doas
 
 nano /etc/apk/repositories
 
-[   Please remove # before all mirorr paths, and save the file  (ctrl+O, ctrl+X)   ]
+# [   Please remove # before all mirorr paths, and save the file  (ctrl+O, ctrl+X)   ]
 
 /etc/apk/repositories must look like this (or you can add these lines to the existing ones) = 
 
@@ -71,7 +71,7 @@ adduser joe wheel
 
 nano /etc/doas.d/doas.conf
 
-Add the following line and save the file  (ctrl+O, ctrl+X):
+# Add the following line and save the file  (ctrl+O, ctrl+X):
 
 permit persist :wheel
 
@@ -104,19 +104,20 @@ rc-update add lightdm
 rc-service lightdm start
 
 
-[for the next command you must be simple user, for that type exit command to exit from root]
+# [for the next command you must be simple user, for that type exit command to exit from root]
 
 exit
 
-[if you have previously created user  ClintEastwood, than type user ClintEastwood, and password for that user]
+# [if you have previously created user  ClintEastwood, than type user ClintEastwood, and password for that user]
 
-[now you can see $ sign]
-
-
-$ echo "exec startxfce4" >> ~/.xinitrc
+# [now you can see $ sign]
 
 
-[that  echo command must be deployed for every user]
+# $ 
+echo "exec startxfce4" >> ~/.xinitrc
+
+
+# [that  echo command must be deployed for every user]
 
 su
 
@@ -125,14 +126,14 @@ reboot
 
 startx
 
-[instead of sudo you can use doas command to start commands as root while you are login as simple user]
+# [instead of sudo you can use doas command to start commands as root while you are login as simple user]
 
-Congratulations, you have successfully installed Alpine Linux.
-Now it remains to take care of the sound.
+# Congratulations, you have successfully installed Alpine Linux.
+# Now it remains to take care of the sound.
 
 
-https://wiki.alpinelinux.org/wiki/ALSA
-Alpine Linux Sound working if you try this:
+# https://wiki.alpinelinux.org/wiki/ALSA
+# Alpine Linux Sound working if you try this:
 
 
 
@@ -146,9 +147,9 @@ apk add pavucontrol
 apk add xfce4-xkb-plugin
 reboot
 
-------------
+# ------------
 
-BTW, some users hate pulseaudio, they like pipewire instead:
+# BTW, some users hate pulseaudio, they like pipewire instead:
 
 apk add pavucontrol pipewire wireplumber pipewire-pulse
 
